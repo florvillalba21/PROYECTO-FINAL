a2611744712a547c744450cad02d3d1e3c05d7dc
florvillalba21 <florvllb33@gmail.com>
2022-11-13 12:02:59 -0300

    agregando algunas cosas de routas 13/11/22

--- PROYECTO-FINAL-FRONT/src/App.jsx
+++ PROYECTO-FINAL-FRONT/src/App.jsx
@@ -1,12 +1,36 @@
-import Sidebar from "./components/Sidebar";
+import { PrivateRoutes } from "../../../tlp2-practico-react/src/routers/PrivateRoutes";
+import { PublicRoutes } from "../../../tlp2-practico-react/src/routers/PublicRoutes";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 import { HomeAdmin } from "./views/HomeAdmin";
+import { Login } from "./views/Login";
 
 function App() {
   return (
-    <>
+    <AuthContext>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRoutes>
+                <Login />
+              </PublicRoutes>
+            }
+          />
 
-      <HomeAdmin />
-    </>
+          <Route
+            path="/*"
+            element={
+              <PrivateRoutes>
+                <HomeAdmin />
+              </PrivateRoutes>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext>
   );
 }
 
--- PROYECTO-FINAL-FRONT/src/views/Login.jsx
+++ PROYECTO-FINAL-FRONT/src/views/Login.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  
+
   return (
     <>
       <section className="vh-100" >
