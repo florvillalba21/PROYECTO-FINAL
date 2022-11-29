b98b538e3116852380c89e480e95ae41d93c60aa
Blito00 <rodriguezpablo0904@gmail.com>
2022-11-28 23:00:24 -0300

    Footer mejorado 23:00hs

--- PROYECTO-FINAL-FRONT/src/ui/Footer.jsx
+++ PROYECTO-FINAL-FRONT/src/ui/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="text-center text-lg-start bg-white text-muted">
       <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         <div className="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
+          <span>Encontranos en todas las redes sociales!</span>
         </div>
 
         <div>
@@ -34,79 +34,30 @@ export const Footer = () => {
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">
-              <i className="bi bi-cone text-secondary"></i>
+              <h6 className="fw-bold mb-4">
+              <i className="bi bi-cone text-secondary">Transit.AR</i>
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                Estámos aquí para poder agilizarte los tiempos en los controles de transito, para que de
+                ésta forma, puedas realizarlos con mayor comodidad.
               </p>
             </div>
 
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Angular
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  React
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Vue
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Laravel
-                </Link>
-              </p>
-            </div>
-
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Pricing
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Settings
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Orders
-                </Link>
-              </p>
-              <p>
-                <Link href="#!" className="text-reset">
-                  Help
-                </Link>
-              </p>
-            </div>
 
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <h6 className="text-uppercase fw-bold mb-4">Contacto:</h6>
               <p>
-                <i className="fas fa-home me-3 text-secondary"></i> New York, NY
-                10012, US
+                <i className="fas fa-home me-3 text-secondary"></i> Ruta Nac. 81 km. 1190
               </p>
               <p>
-                <i className="fas fa-envelope me-3 text-secondary"></i>
-                info@example.com
+                <i className="fas fa-at me-3 text-secondary"></i>
+                transit-AR@support.com
               </p>
               <p>
-                <i className="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88
+                <i className="fas fa-phone me-3 text-secondary"></i> + 54 9 370 480 0818
               </p>
               <p>
-                <i className="fas fa-print me-3 text-secondary"></i> + 01 234 567 89
+                <i className="fas fa-fax me-3 text-secondary"></i> + 54 9 370 484 7478
               </p>
             </div>
           </div>
@@ -117,9 +68,9 @@ export const Footer = () => {
         className="text-center p-4"
         style={{backgroundColor:"rgba(0, 0, 0, 0.025)"}}
       >
-        © 2021 Copyright:
-        <Link className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+        © 2022 Copyright:
+        <Link className="text-reset fw-bold" href="">
+          Transit.AR.com
         </Link>
       </div>
     </footer>
