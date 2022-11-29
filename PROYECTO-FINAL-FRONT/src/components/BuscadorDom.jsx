ea46b2fb1a043c922cd46794a9917ccd820ef777
florvillalba21 <florvllb33@gmail.com>
2022-11-29 16:32:41 -0300

    se agrego un card q mostrará el resultado del input en el buscador de usuarios

--- PROYECTO-FINAL-FRONT/src/components/BuscadorDom.jsx
+++ PROYECTO-FINAL-FRONT/src/components/BuscadorDom.jsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 export const BuscadorDom = () => {
   const inpDom = useRef();
   const [data = {}, setData] = useState({});
+  const [div= " fade hidde", setDiv] = useState()
 
   const dataDom = async (e) => {
     e.preventDefault();
@@ -15,22 +16,55 @@ export const BuscadorDom = () => {
 
       const res = await axios.get(`${url}/${inpDom.current.value}`);
       console.log(data);
+      if(res){
+        setDiv("fade show")
+      }
       setData(res.data);
     } catch (error) {
       console.error("There was an error!", error);
     }
   };
   return (
-    <div className="card text-center" style={{ width: "50%",  margin: "auto", marginTop: "30px" , marginBottom: "30px"}}>
-      <div className="card-header">Transit.Ar</div>
-      <div className="card-body">
-        <h5 className="card-title">Ingresa el dominio que deseas verificar</h5>
-        <input type="text" className="form-control"
-        ref={inpDom}/>
+    <div className="container">
+      <div
+        className="card text-center"
+        style={{
+          width: "50%",
+          margin: "auto",
+          marginTop: "30px",
+          marginBottom: "30px",
+        }}
+      >
+        <div className="card-header">Transit.Ar</div>
+        <div className="card-body">
+          <h5 className="card-title">
+            Ingresa el dominio que deseas verificar
+          </h5>
+          <input type="text" className="form-control" ref={inpDom} />
 
-        <Link onClick={dataDom} to="" className="btn btn-primary" style={{marginTop: "5px"}}>
-          Consultar matrícula
-        </Link>
+          <Link
+            onClick={dataDom}
+            to=""
+            className="btn btn-primary"
+            style={{ marginTop: "5px" }}
+          >
+            Consultar matrícula
+          </Link>
+        </div>
+      </div>
+      <div className={div}>
+        <div
+          className="card text-center"
+          style={{
+            width: "50%",
+            margin: "auto",
+            marginTop: "30px",
+            marginBottom: "30px",
+          }}
+        >
+          <div className="card-header">Resultado de tu búsqueda</div>
+          <div className="card-body" key={data._id}>{data.matricula}</div>
+        </div>
       </div>
     </div>
   );
