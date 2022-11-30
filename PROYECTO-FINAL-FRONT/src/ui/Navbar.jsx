41e7d3d26a56da16a4fbfe1b16a2555165bd2c65
florvillalba21 <florvllb33@gmail.com>
2022-11-30 11:13:04 -0300

    error pull

--- PROYECTO-FINAL-FRONT/src/ui/Navbar.jsx
+++ PROYECTO-FINAL-FRONT/src/ui/Navbar.jsx
@@ -6,15 +6,9 @@ export const Navbar = () => {
     
     <nav className="navbar bg-light">
       <div className="container-fluid">
-<<<<<<< HEAD
-        <h8 className="h6">Todos los derechos reservados®</h8>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-          <button className="btn btn-success">Buscar</button>
-        </form>
-=======
+
         <h6>Todos los derechos reservados®</h6>
->>>>>>> e3cbac39d527c7759e51f46cda2f7a6469ab5068
+
       </div>
     </nav>
     <nav className="navbar bg-dark">
