e3cbac39d527c7759e51f46cda2f7a6469ab5068
Blito00 <rodriguezpablo0904@gmail.com>
2022-11-30 11:09:12 -0300

    Cambios de chill 30-11-22

--- PROYECTO-FINAL-FRONT/src/ui/Navbar.jsx
+++ PROYECTO-FINAL-FRONT/src/ui/Navbar.jsx
@@ -6,61 +6,14 @@ export const Navbar = () => {
     
     <nav className="navbar bg-light">
       <div className="container-fluid">
-        <a className="h6">Todos los derechos reservados®</a>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-          <button className="btn btn-success">Buscar</button>
-        </form>
+        <h6>Todos los derechos reservados®</h6>
       </div>
     </nav>
-    <nav className="navbar navbar-dark navbar-expand-lg" style={{backgroundColor: "black"}}>
+    <nav className="navbar bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" href="#">
-          Bienvenido
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" href="#">
-                Inicio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" href="#">
-                Contacto
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link active dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                ¡Más!
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Action
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+        <h6 style={{color: "white"}}>Bienvenido</h6>
+        <h1><img src="/public/img/TRANSIT-nb.png" style={{width:"120px", marginRight: "700px"}}/></h1>
+
       </div>
     </nav>
     </>
