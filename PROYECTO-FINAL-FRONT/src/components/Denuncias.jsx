5f4ac9df108c93ca1dccb2ca6b6bc1f424a36db6
florvillalba21 <florvllb33@gmail.com>
2022-12-02 11:26:32 -0300

    Solucionado el problema de mostrar denuncias

--- PROYECTO-FINAL-FRONT/src/components/Denuncias.jsx
+++ PROYECTO-FINAL-FRONT/src/components/Denuncias.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 export const ListDenuncias = ({ denuncias =[] }) => {
-  console.log(denuncias)
   if(!denuncias){
     return null
   }
   return (
     <div className="row">
-      {denuncias.map((denuncia, key) => {
-        <div className="col" key={denuncia[key]}>
-          <div className="card">{denuncia[key]}</div>
-        </div>;
-      })}
+      {
+        denuncias.map((denuncia, key) => {
+          return (
+            <div className="col" key={'denuncia' + key}>
+              <div className="card">{denuncia.matricula}</div>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
